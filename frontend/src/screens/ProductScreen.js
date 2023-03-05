0fe6d6b1eac2c2bf7addfcfefea16ae668937586
Sambhu <sambhunath@recruitcrm.io>
2023-03-05 10:42:24 +0530

    Fetching product from the backend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,18 +1,26 @@
-import React from 'react'
-import { Link, useParams } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
-import Rating from '../components/Rating'
-import products from '../products'
-
-
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import Rating from "../components/Rating";
+import axios from "axios";
 
 const ProductScreen = () => {
+    const [product, setProduct] = useState({});
     const params = useParams();
-    const product = products.find((p) => p._id === params.id);
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const { data } = await axios.get(`/api/products/${params.id}`);
+
+            setProduct(data);
+        };
+
+        fetchProduct();
+    }, []);
 
     return (
         <>
-            <Link className='btn btn-light my-3' to='/'>
+            <Link className="btn btn-light my-3" to="/">
                 Go Back
             </Link>
             <Row>
@@ -20,7 +28,7 @@ const ProductScreen = () => {
                     <Image src={product.image} alt={product.name} fluid />
                 </Col>
                 <Col md={3}>
-                    <ListGroup variant='flush'>
+                    <ListGroup variant="flush">
                         <ListGroup.Item>
                             <h3>{product.name}</h3>
                         </ListGroup.Item>
@@ -31,12 +39,14 @@ const ProductScreen = () => {
                             />
                         </ListGroup.Item>
                         <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
-                        <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+                        <ListGroup.Item>
+                            Description: {product.description}
+                        </ListGroup.Item>
                     </ListGroup>
                 </Col>
                 <Col md={3}>
                     <Card>
-                        <ListGroup variant='flush'>
+                        <ListGroup variant="flush">
                             <ListGroup.Item>
                                 <Row>
                                     <Col>Price:</Col>
@@ -49,14 +59,16 @@ const ProductScreen = () => {
                                 <Row>
                                     <Col>Status:</Col>
                                     <Col>
-                                        {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                                        {product.countInStock > 0
+                                            ? "In Stock"
+                                            : "Out Of Stock"}
                                     </Col>
                                 </Row>
                             </ListGroup.Item>
                             <ListGroup.Item>
                                 <Button
-                                    className='btn-block'
-                                    type='button'
+                                    className="btn-block"
+                                    type="button"
                                     disabled={product.countInStock === 0}
                                 >
                                     Add To Cart
@@ -67,7 +79,7 @@ const ProductScreen = () => {
                 </Col>
             </Row>
         </>
-    )
-}
+    );
+};
 
-export default ProductScreen
+export default ProductScreen;
