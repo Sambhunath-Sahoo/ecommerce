da4377eae5e626c61ba5dfe6c551bfd8ed177426
Sambhu <sambhunath@recruitcrm.io>
2023-03-13 23:26:36 +0530

    DB Connection

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import dotenv from 'dotenv';
+import colors from 'colors'
+import connectDB from './config/db.js'
 import products from "./data/products.js";
 
 
 dotenv.config();
+connectDB();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
